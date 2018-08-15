f6411c5a632b00022cf8340b89a2f3cac43ab534
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-15 00:06:00 -0600

    Added sunrise API, UTC conversions to local time, moment.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'; 
+import moment from 'moment'; 
 
 //Sunrise and Sunset API lat and long required and date if date needed is not today's date
 
@@ -9,6 +11,10 @@ class App extends Component {
     this.state = {
       latitude: null, 
       longitude: null,
+      sunriseTodayAPI: null,
+      date: null, 
+      UTCformat :null,
+      localSunriseTime: null, 
     }
   }
 
@@ -22,16 +28,66 @@ class App extends Component {
         this.setState({
           latitude: lat, 
           longitude: long, 
+          date: new Date(),
         })
       }, function (error) { // error callback that produces action in the case that the location is blocked
-        if (error.code == error.PERMISSION_DENIED)
+        if (error.code === error.PERMISSION_DENIED)
             alert("This application requires the use of geolocation to work. Please enable location to proceed");
       })
     }
   }
 
+  // takes in the API call sunrise time as time parameter to transform time to UTC format so it can then be transferred to local.
+  toUTCFormat(time){
+    const timeArr = time.split(' ')
+    let month = this.state.date.getMonth() + 1;
+    let day = this.state.date.getDate();
+    let year = this.state.date.getFullYear();
+
+    if(month.length < 2){
+        month = '0' + month.toString();
+    }
+
+    if(day.length < 2){
+        day = '0' + day.toString();
+    }
+
+    const yearlyDateFormat = [year, month, day].join('-');
+     time = yearlyDateFormat + " " +  timeArr[0]
+
+     this.setState({
+       UTCformat: time,
+     })
+  }
+
+  //from UTCtime to localTime
+  toLocalTime(){
+    const local = moment.utc(this.state.UTCformat).local().format('hh:mm:ss a')
+     this.setState({
+       localSunriseTime: local
+     })
+  }
+
   componentDidMount(){
     this.getGeoLocation();
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.latitude !== this.state.latitude || prevState.longitude !== this.state.longitude){
+      axios.get(`https://api.sunrise-sunset.org/json?lat=${this.state.latitude}&lng=${this.state.longitude}`).then(response => {
+      this.setState({
+          sunriseTodayAPI: response.data.results.sunrise
+        })
+      })
+    }
+    if(prevState.sunriseTodayAPI !== this.state.sunriseTodayAPI) {
+      // const local = moment.utc(this.state.sunriseTodayUTC).local().format('hh:mm:ss a')
+      // console.log(local); 
+      this.toUTCFormat(this.state.sunriseTodayAPI)
+    }
+    if(prevState.UTCformat !== this.state.UTCformat){
+      this.toLocalTime()
+    }
   }
   
   render() {
